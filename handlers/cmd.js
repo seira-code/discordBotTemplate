a58d35df267b9768676050dcdd7aebba9da76a60
seira <78219829+seira-code@users.noreply.github.com>
2022-04-09 15:54:43 +0300

    Made the command loader asynchronous

--- handlers/cmd.js
+++ handlers/cmd.js
@@ -5,13 +5,14 @@ const fs = require('fs')
 */
 module.exports.init = async (client) => {
     console.log("[HANDLER] Cmd handler started!")
-    fs.readdirSync('./commands').forEach(dir => {
-        fs.readdirSync(`./commands/${dir}`).forEach(file => {
+    
+    for(let dir of fs.readdirSync('./commands')) {
+        for(let file of fs.readdirSync(`./commands/${dir}`).filter(x=>x.endsWith(".js"))) {
             const cmd = require(`../commands/${dir}/${file}`)
             client.commandsArray.push(cmd.slash.toJSON())
             client.commands.set(cmd.slash.toJSON().name, cmd)
-        })
-    })
+        }
+    }
 
     client.application.commands.set(client.commandsArray)
 }
