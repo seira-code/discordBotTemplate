42b50cb5641dbadf4895ad4e3a725e0818e5806f
seira <78219829+seira-code@users.noreply.github.com>
2022-04-14 22:05:23 +0300

    Update ready.js

--- events/main/ready.js
+++ events/main/ready.js
@@ -1,17 +1,7 @@
 module.exports = {
     name: 'ready',
     async execute(client) {
-
-        setInterval(() => {
-            let players = 0
-            client.guilds.cache.forEach((gld) => {
-                players += gld.memberCount
-            })
-
-            client.user.setActivity(`U: ${players} | S: ${client.guilds.cache.size}`, { type: "WATCHING" })
-
-        }, 1800000);
-
+        require('../../handlers/cmd.js').init(client)
         console.log(`✨ ${client.user.tag} logged.`);
     }
 }
