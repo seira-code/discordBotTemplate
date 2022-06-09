99cc5e1d68419254ea61245fc9ce8de2e688bab6
seira-code <78219829+seira-code@users.noreply.github.com>
2022-06-09 17:37:13 +0300

    index.js
    update discord.js to 13.8
    set max listeners to infinity

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const client = new Client({
     intents: 4095 // Get intents bitfield here: https://ziad87.net/intents/
 })
+client.setMaxListeners(0)
 
 client.commands = new Collection()
 client.commandsArray = []
@@ -16,8 +17,8 @@ require('./handlers/events.js').init(client)
 
 client.login() // Login with DISCORD_TOKEN on .env
 
-client.on('error',error=>console.log(error))
-client.on('warn',warn=>console.log(warn))
+client.on('error', error => console.log(error))
+client.on('warn', warn => console.log(warn))
 process.on('uncaughtException', console.error);
 process.on('unhandledRejection', console.error);
 
